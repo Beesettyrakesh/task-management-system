276d0189dd27d684ad91377d0b32f3d87fe5344d
rakeshbeesetty <rakeshbeesetty53@gmail.com>
2025-11-16 12:12:56 +0530

    implemented TaskService with CRUD operations

---
+++ src/main/java/com/rakesh/taskmanagement/service/TaskService.java
@@ -0,0 +1,51 @@
+package com.rakesh.taskmanagement.service;
+
+import com.rakesh.taskmanagement.entity.Task;
+import com.rakesh.taskmanagement.exception.ResourceNotFoundException;
+import com.rakesh.taskmanagement.repository.TaskRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class TaskService {
+
+    private TaskRepository taskRepository;
+
+    public TaskService(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
+
+    public Task createTask(Task task) {
+        return taskRepository.save(task);
+    }
+
+    public List<Task> getAllTasks() {
+       List<Task> allTasks = taskRepository.findAll();
+       return allTasks;
+    }
+
+    public Task getTaskById(Long id) {
+        return taskRepository
+                .findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Task not found"));
+    }
+
+    public Task updateTask(Long id, Task task) {
+        if(taskRepository.findById(id).isPresent()) {
+            task.setId(id);
+            return taskRepository.save(task);
+        } else  {
+            throw new ResourceNotFoundException("Task not found");
+        }
+    }
+
+    public void deleteTask(Long id) {
+        if(!taskRepository.existsById(id)) {
+            throw new ResourceNotFoundException("Task not found");
+        }
+        taskRepository.deleteById(id);
+    }
+
+}

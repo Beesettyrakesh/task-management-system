e3483979861cf931186343486ebc4fd32ac11bd4
rakeshbeesetty <rakeshbeesetty53@gmail.com>
2025-11-22 14:07:52 +0530

    Refactor the JwtUtil class for more efficiency

--- src/main/java/com/rakesh/taskmanagement/util/JwtUtil.java
+++ src/main/java/com/rakesh/taskmanagement/util/JwtUtil.java
@@ -7,23 +7,30 @@ import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+
 import java.util.Date;
 
 @Component
 public class JwtUtil {
 
     @Value("${jwt.secret}")
-    private byte[]  jwtSecretBase64;
+    private String  jwtSecretBase64;
 
     @Value("${jwt.expiration}")
     private Long expirationTime;
+
+    private SecretKey getSecretKey() {
+        return Keys.hmacShaKeyFor(jwtSecretBase64.getBytes(StandardCharsets.UTF_8));
+    }
 
     public String generateToken(String username) {
         return Jwts.builder()
                 .subject(username)
                 .expiration(new Date(System.currentTimeMillis() + expirationTime))
                 .issuedAt(new Date(System.currentTimeMillis()))
-                .signWith(Keys.hmacShaKeyFor(jwtSecretBase64), Jwts.SIG.HS256)
+                .signWith(getSecretKey(), Jwts.SIG.HS256)
                 .compact();
     }
 
@@ -45,7 +52,7 @@ public class JwtUtil {
 
     public Claims extractClaims(String token) {
         return Jwts.parser()
-                .verifyWith(Keys.hmacShaKeyFor(jwtSecretBase64))
+                .verifyWith(getSecretKey())
                 .build()
                 .parseSignedClaims(token)
                 .getPayload();
